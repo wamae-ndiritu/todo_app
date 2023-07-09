5c74fc66740d8b00e8868fb2f7488eab1e5b924e
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-07-09 18:22:06 +0300

    feat: add new inputs form

--- client/src/Todo/Todo.jsx
+++ client/src/Todo/Todo.jsx
@@ -2,6 +2,13 @@ import React, { useState, useEffect } from "react";
 import "./Todo.css";
 
 function Todo() {
+  const [notes, setNotes] = useState({ title: "", description: "" });
+
+  const handleChange = (e) => {
+    setNotes({ ...notes, [e.target.name]: e.target.value });
+  };
+
+  console.log(notes);
   // const handleSubmit = (e)=>{
   //   e.preventDefault()
   // }
@@ -10,26 +17,22 @@ function Todo() {
       <div className="wrapper">
         <h2 className="title">Todo List</h2>
         <div className="input-cont">
-          <textarea rows={4}></textarea>
+          <h5>Note Title</h5>
+          <input type="text" name="title" onChange={handleChange} />
+          <h5>Note Description</h5>
+          <textarea
+            rows={3}
+            name="description"
+            onChange={handleChange}
+          ></textarea>
           <button>Add Todo</button>
         </div>
         <div className="todo-items">
           <div className="todo-item">
-            <h6>Note 1</h6>
-            <div className="actions">
-              <div className="action">
-                <i className="fa fa-eye" aria-hidden="true"></i>
-              </div>
-              <div className="action">
-                <i className="fa fa-pencil-square-o" aria-hidden="true"></i>
-              </div>
-              <div className="action">
-                <i className="fa fa-trash" aria-hidden="true"></i>
-              </div>
+            <div>
+              <h6>Live Coding session</h6>
+              <p>We are having a live coding session.</p>
             </div>
-          </div>
-          <div className="todo-item">
-            <h6>We are doing some live coding</h6>
             <div className="actions">
               <div className="action">
                 <i className="fa fa-eye" aria-hidden="true"></i>
